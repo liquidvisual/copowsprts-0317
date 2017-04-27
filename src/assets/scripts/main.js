e20b6366fa6bd1bd8f5033a39c1f22a5fa15c88b
Mitchell Warren <hello@liquidvisual.com>
2017-04-27 16:28:28 +1000

    Lightbox update

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -15,7 +15,32 @@ $(function() {
         $('html').addClass('has-loaded');
         $('input, textarea').placeholder(); // IE9 Patch
         $('[data-masonry]').masonry();
+        lightbox();
     });
+
+    //-----------------------------------------------------------------
+    // POPUP GALLERY
+    //-----------------------------------------------------------------
+
+    function lightbox() {
+        $('.popup-gallery').magnificPopup({
+            delegate: 'li > a',
+            type: 'image',
+            tLoading: 'Loading image #%curr%...',
+            mainClass: 'mfp-img-mobile',
+            gallery: {
+                enabled: true,
+                navigateByImgClick: true,
+                preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+            },
+            image: {
+                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+                titleSrc: function(item) {
+                    return item.el.attr('title') + '<small></small>';
+                }
+            }
+        });
+    }
 
     //-----------------------------------------------------------------
     // LAUNCH MANAGE ON KEYPRESS
