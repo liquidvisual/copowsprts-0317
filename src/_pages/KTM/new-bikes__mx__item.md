2fe3eb59a748adc4ff2f4110e8a3f503e12f690b
Mitchell Warren <hello@liquidvisual.com>
2017-05-11 16:47:21 +1000

    Updated JS for performance

--- src/_pages/KTM/new-bikes__mx__item.md
+++ src/_pages/KTM/new-bikes__mx__item.md
@@ -1,10 +1,10 @@
 ---
 layout: child_layout/brand_categories_product_item
-title: 450 SX-F Factory Edition
-permalink: /ktm/new-bikes/mx/450-sx-f-factory-edition/
+title: Freeride 350 2017
+permalink: /ktm/new-bikes/freeride/freeride-350-2017/
 hero: /assets/img/content/hero-2.jpg
 logo: /assets/img/content/logo-ktm.png
-learner_approved: true
+learner_approved: false
 breadcrumbs: true
 brand: ktm
 theme: theme-ktm
@@ -12,85 +12,123 @@ theme: theme-ktm
 media:
   - title: Bike product shot
     url: /assets/img/content/product-shot-1.jpg
-
-  - title: Bike product shot
-    url: /assets/img/content/product-shot-2.jpg
-
-  - title: Bike product shot
-    url: /assets/img/content/product-shot-3.jpg
 ---
 
-## We don't just build replicas, we build factory editions
+## The High Flier
 
-Championship winning bikes, like this one. Thanks to its compact SOHC (single overhead camshaft) engine delivering explosive but controllable power, it is the weapon of choice for our Factory Teams around the world. Experts will notice its revolutionary WP AER 48 forks, the anodized triple clamps and sprocket, its upgraded Akrapovič muffler, the orange frame or the D.I.D DirtStar rims. The rest will only see a blur blasting past. Is it Ryan Dungey? Possibly. Or it could be you.
+<p><span>Freedom has a name: on the FREERIDE 350, only the rider determines the route. Since its launch, it impressed everyone who had always wanted to ride offroad "for the heck of it", without any pressure to perform. For the upcoming model year, KTM is focussing clearly with its further refinement of the FREERIDE 350 on increased all-round potential and enhanced suitability for everyday use. For example, an extended top gear and a longer final transmission ratio lower the revs and reduce fuel consumption along highway sections, while the shortened five lower gears as ever guarantee optimum power delivery offroad. Numerous optimized details further improve the durability and reliability of the new FREERIDE 350 and contribute to reducing its already extraordinarily low weight even further.</span></p>
 
-<br>
+<p><br></p>
 
-### Engine
-
-<table>
-	<thead hidden>
-		<tr>
-			<th>Item</th>
-			<th>Description</th>
-		</tr>
-	</thead>
-	<tbody>
-		<tr>
-			<th>Design</th>
-			<td>1-cylinder 2-stroke engine, water-cooled, with reed intake</td>
-		</tr>
-		<tr>
-			<th>Design</th>
-			<td>1-cylinder 2-stroke engine, water-cooled, with reed intake</td>
-		</tr>
-		<tr>
-			<th>Displacement</th>
-			<td>49.0&nbsp;cm³</td>
-		</tr>
-		<tr>
-			<th>Bore</th>
-			<td>39.5&nbsp;mm</td>
-		</tr>
-		<tr>
-			<th>Stroke</th>
-			<td>40&nbsp;mm</td>
-		</tr>
-		<tr>
-			<th>Starting aid</th>
-			<td>Kickstarter</td>
-		</tr>
-		<tr>
-			<th>Transmission</th>
-			<td>Rigid 1-stage reduction gear</td>
-		</tr>
-		<tr>
-			<th>Engine lubrication</th>
-			<td>Separate lubrication</td>
-		</tr>
-		<tr>
-			<th>Primary gear ratio</th>
-			<td>33:61 straight cut spur gear</td>
-		</tr>
-		<tr>
-			<th>Final drive</th>
-			<td>10:42</td>
-		</tr>
-		<tr>
-			<th>Cooling system</th>
-			<td>Liquid cooling system</td>
-		</tr>
-		<tr>
-			<th>Clutch</th>
-			<td>Multidisc automatic clutch on the main shaft / centrifugal force</td>
-		</tr>
-		<tr>
-			<th>Ignition system</th>
-			<td>SELETTRA 2p D36</td>
-		</tr>
-		<tr>
-			<th>Total fuel tank capacity approx.</th>
-			<td>2.0&nbsp;l<br> Unleaded premium fuel (95 RON)</td>
-		</tr>
-	</tbody>
-</table>
+<div class="specifications">
+	<h2>ENGINE</h2>
+	<ul class="tech-details">
+		<li>
+			<h3>DESIGN</h3>
+			<p>1-cylinder, 4-stroke engine</p>
+		</li>
+		<li>
+			<h3>DISPLACEMENT</h3>
+			<p>349.7 cm³</p>
+		</li>
+		<li>
+			<h3>BORE</h3>
+			<p>88 mm</p>
+		</li>
+		<li>
+			<h3>STROKE</h3>
+			<p>57.5 mm</p>
+		</li>
+		<li>
+			<h3>STARTER</h3>
+			<p>Electric starter</p>
+		</li>
+		<li>
+			<h3>TRANSMISSION</h3>
+			<p>6-speed</p>
+		</li>
+		<li>
+			<h3>PRIMARY DRIVE</h3>
+			<p>24:73</p>
+		</li>
+		<li>
+			<h3>SECONDARY GEAR RATIO</h3>
+			<p>12:48</p>
+		</li>
+		<li>
+			<h3>CLUTCH</h3>
+			<p>Wet, multi-disc clutch, Formula hydraulics</p>
+		</li>
+		<li>
+			<h3>EMS</h3>
+			<p>Keihin EMS</p>
+		</li>
+	</ul>
+	<h2>CHASSIS</h2>
+	<ul class="tech-details">
+		<li>
+			<h3>FRAME DESIGN</h3>
+			<p>Perimeter steel-aluminium composite frame</p>
+		</li>
+		<li>
+			<h3>FRONT SUSPENSION</h3>
+			<p>WP upside-down Ø 43&nbsp;mm</p>
+		</li>
+		<li>
+			<h3>REAR SUSPENSION</h3>
+			<p>WP PDS shock absorber</p>
+		</li>
+		<li>
+			<h3>SUSPENSION TRAVEL (FRONT)</h3>
+			<p>250 mm</p>
+		</li>
+		<li>
+			<h3>SUSPENSION TRAVEL (REAR)</h3>
+			<p>260 mm</p>
+		</li>
+		<li>
+			<h3>FRONT BRAKE</h3>
+			<p>Disc brake</p>
+		</li>
+		<li>
+			<h3>REAR BRAKE</h3>
+			<p>Disc brake</p>
+		</li>
+		<li>
+			<h3>FRONT BRAKE DISC DIAMETER</h3>
+			<p>260 mm</p>
+		</li>
+		<li>
+			<h3>REAR BRAKE DISC DIAMETER</h3>
+			<p>210 mm</p>
+		</li>
+		<li>
+			<h3>CHAIN</h3>
+			<p>X-Ring 5/8 x 1/4"</p>
+		</li>
+		<li>
+			<h3>STEERING HEAD ANGLE</h3>
+			<p>67 °</p>
+		</li>
+		<li>
+			<h3>WHEELBASE</h3>
+			<p>1418 ± 10 mm</p>
+		</li>
+		<li>
+			<h3>GROUND CLEARANCE</h3>
+			<p>325 mm</p>
+		</li>
+		<li>
+			<h3>SEAT HEIGHT</h3>
+			<p>915 mm</p>
+		</li>
+		<li>
+			<h3>TANK CAPACITY (APPROX.)</h3>
+			<p>5.5 l</p>
+		</li>
+		<li>
+			<h3>WEIGHT READY TO RACE (WITHOUT FUEL)</h3>
+			<p>99.5 kg</p>
+		</li>
+	</ul>
+</div>
--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,115 +2,104 @@
     MAIN.JS - Last updated: 06.10.16
 */
 //-----------------------------------------------------------------
-//
+// ON LOAD
 //-----------------------------------------------------------------
 
-$(function() {
+$(window).on('load', function() {
+    $('html').addClass('has-loaded');
+    $('[data-masonry]').masonry();
+    lightbox();
+});
 
-    //-----------------------------------------------------------------
-    // WINDOW LOAD
-    //-----------------------------------------------------------------
+//-----------------------------------------------------------------
+// POPUP GALLERY
+//-----------------------------------------------------------------
 
-    $(window).on('load', function() {
-        $('html').addClass('has-loaded');
-        $('input, textarea').placeholder(); // IE9 Patch
-        $('[data-masonry]').masonry();
-        lightbox();
-    });
-
-    //-----------------------------------------------------------------
-    // POPUP GALLERY
-    //-----------------------------------------------------------------
-
-    function lightbox() {
-        $('.popup-gallery').magnificPopup({
-            delegate: 'li > a',
-            type: 'image',
-            tLoading: 'Loading image #%curr%...',
-            mainClass: 'mfp-img-mobile',
-            gallery: {
-                enabled: true,
-                navigateByImgClick: true,
-                preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-            },
-            image: {
-                tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-                titleSrc: function(item) {
-                    return item.el.attr('title') + '<small></small>';
-                }
+function lightbox() {
+    $('.popup-gallery').magnificPopup({
+        delegate: 'li > a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function(item) {
+                return item.el.attr('title') + '<small></small>';
             }
-        });
-    }
-
-    //-----------------------------------------------------------------
-    // LAUNCH MANAGE ON KEYPRESS
-    //-----------------------------------------------------------------
-
-    key('⌘+shift+m, ctrl+shift+m', function(){
-      window.location = '/manage/';
+        }
     });
+}
 
-    //-----------------------------------------------------------------
-    // MATCH HEIGHT
-    //-----------------------------------------------------------------
+//-----------------------------------------------------------------
+// LAUNCH MANAGE ON KEYPRESS
+//-----------------------------------------------------------------
 
-    $('[data-match-height]').matchHeight(
-     {
-         byRow: false,
-         property: 'height',
-         target: null,
-         remove: false,
-         mq: "(min-width: 768px)"
-     });
+key('⌘+shift+m, ctrl+shift+m', function(){
+  window.location = '/manage/';
+});
 
-    //-----------------------------------------------------------------
-    // HEADROOM.js
-    //-----------------------------------------------------------------
+//-----------------------------------------------------------------
+// MATCH HEIGHT
+//-----------------------------------------------------------------
 
-    $(".global-header").headroom({
-        // vertical offset in px before element is first unpinned
-        offset : 52,
-        // scroll tolerance in px before state changes
-        tolerance : 0,
-        // or you can specify tolerance individually for up/down scroll
-        tolerance : {
-            up : 5,
-            down : 0
-        },
-        // css classes to apply
-        classes : {
-            // when element is initialised
-            initial : "headroom",
-            // when scrolling up
-            pinned : "headroom--pinned",
-            // when scrolling down
-            unpinned : "headroom--unpinned",
-            // when above offset
-            top : "headroom--top",
-            // when below offset
-            notTop : "headroom--not-top",
-            // when at bottom of scoll area
-            bottom : "headroom--bottom",
-            // when not at bottom of scroll area
-            notBottom : "headroom--not-bottom"
-        },
-        // element to listen to scroll events on, defaults to `window`
-        // scroller : someElement,
-        // callback when pinned, `this` is headroom object
-        onPin : function() {},
-        // callback when unpinned, `this` is headroom object
-        onUnpin : function() {},
-        // callback when above offset, `this` is headroom object
-        onTop : function() {},
-        // callback when below offset, `this` is headroom object
-        onNotTop : function() {},
-        // callback when at bottom of page, `this` is headroom object
-        onBottom : function() {},
-        // callback when moving away from bottom of page, `this` is headroom object
-        onNotBottom : function() {}
-    });
+$('[data-match-height]').matchHeight({
+     byRow: false,
+     property: 'height',
+     target: null,
+     remove: false,
+     mq: "(min-width: 768px)"
+ });
 
-//--
+//-----------------------------------------------------------------
+// HEADROOM.js
+//-----------------------------------------------------------------
+
+$(".global-header").headroom({
+    // vertical offset in px before element is first unpinned
+    offset : 52,
+    // scroll tolerance in px before state changes
+    tolerance : 0,
+    // or you can specify tolerance individually for up/down scroll
+    tolerance : {
+        up : 5,
+        down : 0
+    },
+    // css classes to apply
+    classes : {
+        // when element is initialised
+        initial : "headroom",
+        // when scrolling up
+        pinned : "headroom--pinned",
+        // when scrolling down
+        unpinned : "headroom--unpinned",
+        // when above offset
+        top : "headroom--top",
+        // when below offset
+        notTop : "headroom--not-top",
+        // when at bottom of scoll area
+        bottom : "headroom--bottom",
+        // when not at bottom of scroll area
+        notBottom : "headroom--not-bottom"
+    },
+    // element to listen to scroll events on, defaults to `window`
+    // scroller : someElement,
+    // callback when pinned, `this` is headroom object
+    onPin : function() {},
+    // callback when unpinned, `this` is headroom object
+    onUnpin : function() {},
+    // callback when above offset, `this` is headroom object
+    onTop : function() {},
+    // callback when below offset, `this` is headroom object
+    onNotTop : function() {},
+    // callback when at bottom of page, `this` is headroom object
+    onBottom : function() {},
+    // callback when moving away from bottom of page, `this` is headroom object
+    onNotBottom : function() {}
 });
 
 //==================================================
